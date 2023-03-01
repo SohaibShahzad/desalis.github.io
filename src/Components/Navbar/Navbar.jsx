3708815c93ecc23448349d58d0e66d48c79f2887
Hassaan Ahmad <2020CS678@student.uet.edu.pk>
2023-03-01 09:15:27 +0500

    Final client site updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -386,7 +386,7 @@ const Navbar = ({ list }) => {
           <div className="row">
             <div className="col-12">
               <nav className={style.main_nav}>
-                <Link to="/" className={style.logo}>
+                <Link to="/" className={`${style.logo} ms-4`}>
                   <img
                     alt="logo"
                     style={{ width: "100%", height: "100%" }}
