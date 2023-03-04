8632068fb953b1de5fa5ba5f300fe146900342f1
Hassaan Ahmad <2020CS678@student.uet.edu.pk>
2023-03-04 22:03:12 +0500

    Footer responsive

--- src/Components/mailList/MailList.jsx
+++ src/Components/mailList/MailList.jsx
@@ -3,17 +3,19 @@ import { useMediaQuery } from "@mui/material";
 
 const MailList = () => {
   const isXtraSmallScreen = useMediaQuery("(max-width: 450px)");
+  console.log(isXtraSmallScreen);
   return (
-    <div className={`mail ${isXtraSmallScreen?'align-items-center':''} `}>
-      <h1 className={`text-start fs-4 text-uppercase`} >News Letter</h1>
-      <h1 className="mailTitle text-start fs-5" >Save time, save money!</h1>
-      <span className="mailDesc text-start " >Sign up and get the best deals</span>
-      <div className="mailInputContainer px-5 ">
-        <input type="email" className="btn-block" placeholder="Your Email" />
+    <div className={`mail mt-2 ${isXtraSmallScreen ? "align-items-center" : ""} `}>
+      <h1 className={`text-start fs-4 text-uppercase`}>News Letter</h1>
+      <h1 className="mailTitle text-start fs-5">Save time, save money!</h1>
+      <span className="mailDesc text-start ">
+        Sign up and get the best deals
+      </span>
+      <div className="mailInputContainer mt-3">
+        <input type="email" placeholder="Your Email" />
         <button className="btn btn-primary btn-block">Subscribe</button>
       </div>
     </div>
-    
   );
 };
 
